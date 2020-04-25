a2f765eeae2a5a42636d58e680457931974db571
gucchisk <gucchi_sk@yahoo.co.jp>
2020-04-25 18:40:43 +0900

    Update run script

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # PDF をとってくる
+echo "pipenv run ./fetch_tokyo_covid_report_pdf.py"
 NEW_PDF_FILE=$(pipenv run ./fetch_tokyo_covid_report_pdf.py)
 if [[ -z "${NEW_PDF_FILE}" ]] ;then
   echo "No new PDF. exited"
@@ -11,10 +12,8 @@ fi
 NEW_CSV_FILE=${NEW_PDF_FILE//pdf/csv}
 
 echo "pipenv run ./parse_tokyo_covid_report_pdf.py ${NEW_PDF_FILE} > ${NEW_CSV_FILE}"
-
 pipenv run ./parse_tokyo_covid_report_pdf.py ${NEW_PDF_FILE} > ${NEW_CSV_FILE}
 
-
-
-# latest.csv のコピーを生成
-# cp "${NEW_CSV_FILE}" csv/latest.csv
+# list.txtを更新
+echo "./update_list.py"
+./update_list.py
